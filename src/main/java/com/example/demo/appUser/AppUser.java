758892ed368b61febb4c200d6260ce05632766fa
BoHaoTing <a405047030@gmail.com>
2022-06-10 17:55:45 +0800

    create appUser entity and registration. build spring security config.

---
+++ src/main/java/com/example/demo/appUser/AppUser.java
@@ -0,0 +1,87 @@
+package com.example.demo.appUser;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+public class AppUser implements UserDetails {
+
+    @Id
+    @SequenceGenerator(
+            name = "appuser_sequence",
+            sequenceName = "appuser_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "appuser_sequence"
+    )
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+    private Boolean locked;
+    private Boolean enabled;
+
+    public AppUser(String username, String password, String name, String email, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(AppUserRole.USER.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.example.demo.registration;
+
+
+public class RegistrationService {
+
+
+    public String register(RegistrationRequest request) {
+        return "works.";
+    }
+}
